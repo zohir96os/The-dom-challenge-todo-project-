67e8b8016b2fe76e4baa2f920428ff34ee6719d9
zohiros <zohir96os@gmail.com>
2024-01-30 23:07:31 +0100

    modded

--- index.js
+++ index.js
@@ -3,51 +3,49 @@ let inputField = document.querySelector(".todo-list");
 let inputText = document.querySelector(".taskInput");
 let add = document.querySelector(".add");
 
-let addNewTask = (e) => {
-  //prevent the default event
-  e.preventDefault();
-  if (inputText.value !== "" && inputText.value.trim() !== "") {
-    //create new field
-    let newInputField = document.createElement("div");
-    //add class to the created field
-    newInputField.classList.add("input-area");
-    //creat button delete and edit
-    let edit = document.createElement("button");
-    edit.innerText = "Edit";
-    edit.classList.add("edit");
-    let remove = document.createElement("button");
-    remove.innerText = "Remove";
-    remove.classList.add("remove");
-    //appden btns to the inpute
-    newInputField.append(edit, remove);
-    //create new input element
-    let taskInput = document.createElement("input");
-    taskInput.type = "text";
-    taskInput.disabled = true;
-    taskInput.value = inputText.value;
-    //append the created inpute to the div
-    newInputField.prepend(taskInput);
-    //add the taskInput to the dom
-    inputField.prepend(newInputField);
-    //set input field empty
-    inputText.value = "";
-    //handle remove and edit buttons
-    remove.addEventListener("click", removeTask);
-    edit.addEventListener("click", editTask);
-    // Save the task to local storage
-    saveTaskToLocalStorage(inputText.value);
-  }
+//Create new task function
+const creatTaskElement = (taskText) => {
+  let taskElement = document.createElement("div");
+  taskElement.classList.add("input-area");
+
+  const edit = document.createElement("button");
+  edit.innerText = "Edit";
+  edit.classList.add("edit");
+  const remove = document.createElement("button");
+  remove.innerText = "Remove";
+  remove.classList.add("remove");
+
+  taskElement.append(edit, remove);
+
+  const taskInput = document.createElement("input");
+  taskInput.type = "text";
+  taskInput.disabled = true;
+  taskInput.value = taskText;
+
+  taskElement.prepend(taskInput);
+
+  remove.addEventListener("click", removeTask);
+  edit.addEventListener("click", editTask);
+
+  return taskElement;
 };
 
+let addNewTask = (e) => {
+  e.preventDefault();
+
+  const trimmedInput = inputText.value.trim();
+  if (trimmedInput !== "") {
+    const newTaskElement = creatTaskElement(trimmedInput);
+    inputField.prepend(newTaskElement);
+    inputText.value = "";
+  }
+};
+//Remove task function
 let removeTask = (e) => {
   // Remove the task from the DOM
   e.target.parentElement.remove();
 };
-
-// const saveTaskToLocalStorage = localStorage.setItem("myCat", "Tom");
-// const getTaskFromLocalStorage = localStorage.getItem("myCat");
-// const removeTaskFromLocalStorage = localStorage.removeItem("myCat");
-
+//Edit task function
 let editTask = (e) => {
   let input = e.target.parentNode.firstChild;
   input.disabled = false;
@@ -55,6 +53,7 @@ let editTask = (e) => {
   e.target.parentNode.remove();
 };
 
+//Handle enter key button
 add.addEventListener("click", addNewTask);
 inputText.addEventListener("keyup", function (event) {
   if (event.key === "Enter") {
